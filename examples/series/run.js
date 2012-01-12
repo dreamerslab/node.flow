ec702f71d55b5c2dc0ee01932695eab1cffa42b0
ben <ben@dreamerslab.com>
2012-01-13 02:12:41 +0800

    rename example simple to series

--- examples/series/run.js
+++ examples/series/run.js
@@ -3,17 +3,17 @@ var flow = new Flow( 5, 6, 7 );
 
 
 
-flow.add( function ( x, y, z, next ){
+flow.series( function ( x, y, z, next ){
   // simulates a time consuming io operation
   setTimeout( function (){
-    console.log( '\nfirst task ---------------' );
+    console.log( 'first task ---------------' );
     // default value of x is 5, now is overwritten to 7
     console.log( 'x : ' + x );
     console.log( 'y : ' + y );
     console.log( 'z : ' + z + '\n' );
     next( 11 );
   }, 200 );
-}, 7 ).add( function ( x, y, z, next ){
+}, 7 ).series( function ( x, y, z, next ){
   setTimeout( function (){
     console.log( 'second task ---------------' );
     // default value of x is 5, and was overwritten to 9
