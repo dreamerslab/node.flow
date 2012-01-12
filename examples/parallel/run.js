30f7aa23c84da6ba57ae10fd06c261bf695f0306
ben <ben@dreamerslab.com>
2012-01-13 02:11:58 +0800

    add parallel example

---
+++ examples/parallel/run.js
@@ -0,0 +1,36 @@
+var Flow = require( '../../lib/flow' );
+var flow = new Flow( 5, 6, 7 );
+
+
+
+flow.parallel( function ( x, y, z, ready ){
+  // simulates a time consuming io operation
+  setTimeout( function (){
+    console.log( 'first task ---------------' );
+    // default value of x is 5, now is overwritten to 7
+    console.log( 'x : ' + x );
+    console.log( 'y : ' + y );
+    console.log( 'z : ' + z + '\n' );
+    ready( 11 );
+  }, 200 );
+}, 7 ).parallel( function ( x, y, z, ready ){
+  setTimeout( function (){
+    console.log( 'second task ---------------' );
+    // default value of x is 5, and was overwritten to 9
+    // but finally is overwritten from previous stack `next` to 11
+    console.log( 'x : ' + x );
+    console.log( 'y : ' + y );
+    console.log( 'z : ' + z + '\n' );
+    ready( 1000 );
+  }, 100 );
+}, 9, 10, 55 ).join().
+end( function ( from_parallel, x, y, z, next ){
+  setTimeout( function (){
+    console.log( 'all finished callback ---------------' );
+    console.log( 'par1 : ' + from_parallel[ 0 ][ 0 ] );
+    console.log( 'par2 : ' + from_parallel[ 1 ][ 0 ] );
+    console.log( 'x : ' + x );
+    console.log( 'y : ' + y );
+    console.log( 'z : ' + z + '\n' );
+  }, 150 );
+}, 700 );
